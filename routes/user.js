cbfb089dd2699d32505a832ee74373b765ebbf53
omar-momen <omarelzhery236@gmail.com>
2025-08-12 23:15:58 +0300

    .

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,7 @@ import {
   resetPassword,
   updatePassword,
   protect,
+  restrictTo,
 } from '../controllers/auth.js';
 import {
   getAllUsers,
@@ -25,18 +26,24 @@ router.post('/login', login);
 router.post('/forgotPassword', forgotPassword);
 router.patch('/resetPassword/:token', resetPassword);
 
-router.patch('/updatePassword', protect, updatePassword);
-router.patch('/updateMe', protect, updateMe);
-router.delete('/deleteMe', protect, deleteMe);
-router.get('/getMe', protect, getMe);
+// Protect all routes after this middleware
+router.use(protect);
+
+router.patch('/updatePassword', updatePassword);
+router.patch('/updateMe', updateMe);
+router.delete('/deleteMe', deleteMe);
+router.get('/getMe', getMe);
 
 router.route('/')
   .get(getAllUsers)
   .post(createUser);
 
+// Restrict all routes after this middleware to admin
+router.use(restrictTo('admin'));
+
 router.route('/:id')
-  .get(protect, restrictTo('admin'), getUser)
-  .patch(protect, restrictTo('admin'), updateUser)
-  .delete(protect, restrictTo('admin'), deleteUser);
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);
 
 export default router;
